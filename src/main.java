80c874b87ed90ac9cd1923ebdd7845fc54cbfc2e
cristhianfigueredo2003-source <cristhianfigueredo2003@gmail.com>
2026-03-27 14:57:20 -0300

    AGREGADA FUNCION SALUDAR

--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Cambio de Prueba para git");
+        saludar("Cristian");
+    }
+
+    public static void saludar(String nombre) {
+        System.out.println("Hola " + nombre);
     }
 }
